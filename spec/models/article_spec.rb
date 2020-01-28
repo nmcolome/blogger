e3f124af934101d6e3c9b7fd6d8318cfcfbc57a9
NataliaColome <nmcolome@gmail.com>
2020-01-28 16:11:53 -0700

    Add Article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Article' do
+RSpec.describe Article do
   describe 'validations' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :body }
