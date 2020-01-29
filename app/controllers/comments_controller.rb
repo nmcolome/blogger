83113fb189d60389d38949df6433a618f4cf7853
NataliaColome <nmcolome@gmail.com>
2020-01-29 00:27:42 -0700

    Add Comments #create action

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+
+    @comment.save
+
+    redirect_to article_path(params[:article_id])
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:author_name, :body)
+    end
+end
