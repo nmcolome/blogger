424533daefdab0caab74a9572014aafab9df6697
NataliaColome <nmcolome@gmail.com>
2020-01-29 00:50:29 -0700

    Add model spec, test relationships for Tag, Tagging & Article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Article do
   describe 'relatioships' do
-    it { should have_many :comments}
+    it { should have_many :comments }
+    it { should have_many :taggings }
+    it { should have_many :tags, through: :taggings }
   end
 
   describe 'validations' do
