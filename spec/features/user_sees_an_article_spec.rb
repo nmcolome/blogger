86275a78edc47e13a56b8fa3553b0142e43fe3bf
NataliaColome <nmcolome@gmail.com>
2020-01-28 23:38:45 -0700

    Add feature spec for comment form in article show page

--- spec/features/user_sees_an_article_spec.rb
+++ spec/features/user_sees_an_article_spec.rb
@@ -20,4 +20,21 @@ RSpec.describe "User sees an article" do
       expect(page).to have_content(comment2.body)
     end
   end
+
+  describe "they fill in a comment form" do
+    it "displays the comment on the article show" do
+      article = Article.create!(title:"Title", body:"Body")
+
+      visit article_path(article)
+
+      fill_in "comment[author_name]", with: "Me"
+      fill_in "comment[body]", with: "My opinion"
+      click_on "Submit"
+
+      expect(current_path).to eq(article_path(article))
+      expect(page).to have_content("Post a Comment")
+      expect(page).to have_content("Me")
+      expect(page).to have_content("My opinion")
+    end
+  end
 end
