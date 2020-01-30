bedb882d4608fa469003a83184f421e748ca55fa
NataliaColome <nmcolome@gmail.com>
2020-01-29 23:54:34 -0700

    Sorcery authentication complete

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,7 @@
 class AuthorsController < ApplicationController
   before_action :set_author, only: [:show, :edit, :update, :destroy]
   before_action :zero_authors_or_authenticated, only: [:new, :create]
+  before_action :require_login, except: [:new, :create]
 
   # GET /authors
   # GET /authors.json
@@ -75,6 +76,13 @@ class AuthorsController < ApplicationController
 
     def zero_authors_or_authenticated
       unless Author.count == 0 || current_user
+        redirect_to root_path
+        return false
+      end
+    end
+
+    def require_login
+      unless current_user
         redirect_to root_path
         return false
       end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,7 @@
 class TagsController < ApplicationController
   before_action :set_tag, only: [:show, :destroy]
+  before_action :require_login, only: [:destroy]
+
   def index
     @tags = Tag.all
   end
@@ -17,5 +19,12 @@ class TagsController < ApplicationController
   private
     def set_tag
       @tag = Tag.find(params[:id])
+    end
+
+    def require_login
+      unless current_user
+        redirect_to root_path
+        return false
+      end
     end
 end
